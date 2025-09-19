05db1d28c4140819f65a71b4ee478e2b5b7e83d2
dkisavi <denis.kisavi@zeraki.app>
2025-09-19 10:21:22 +0300

    add firebase sdk

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAbfzLqLnlcNNI7-0N4GgQgKNUcHvsVi-U",
+        authDomain: "quantum-till.firebaseapp.com",
+        projectId: "quantum-till",
+        storageBucket: "quantum-till.firebasestorage.app",
+        messagingSenderId: "573303586613",
+        appId: "1:573303586613:web:e9246f852fc79e8632567c",
+        measurementId: "G-TM1RXL4JL7"
+    }
+}
